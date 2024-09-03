9f0626f07e0674a090caf9ab8fd163dc51ebe47b
yuanshui <1195131743@qq.com>
2024-09-03 17:55:49 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,6 +13,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2!");
-        System.out.println("hello git3!");
+        System.out.println("hello git4!");
     }
 }

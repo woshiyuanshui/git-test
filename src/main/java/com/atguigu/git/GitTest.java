cde116001533fe3a10274a75a8d2b9c93d2f9114
yuanshui <1195131743@qq.com>
2024-09-03 17:44:31 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * ClassName: GitTest
+ * Package: com.atguigu.git
+ * Description:
+ *
+ * @Author: 远水
+ * @Create: 2024/9/3 - 17:41
+ * Version: v1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}

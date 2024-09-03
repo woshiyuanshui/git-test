8d045de03c48ee9e627a1b32bf222c4db6b6ed80
yuanshui <1195131743@qq.com>
2024-09-03 18:06:28 +0800

    master commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,6 +13,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2!");
+        System.out.println("hello git3!");
         System.out.println("hello git4!");
+        System.out.println("master test!");
     }
 }
